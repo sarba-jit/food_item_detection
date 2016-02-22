5d1265bd1515cfeaf1b8f9a848297b874d33768d
sarba-jit <sarbajit.mukherjee@aggiemail.usu.edu>
2016-02-22 14:19:43 -0700

    contour condition added

--- sauce_detect.py
+++ sauce_detect.py
@@ -4,6 +4,8 @@ import os
 import time
 
 start_time = time.time()
+
+
 # Function for sorting the lengths of the contour list
 def list_sorted_length(number_list):
     for item1 in range(0, len(number_list)):
@@ -28,14 +30,14 @@ def list_sorted(number_list):
 
 ##Function for finding the contours
 def contour_finding(gray_image):
-    test_path = "rice_test/new_images_2_17/results_gray/"
+    test_path = "rice_test/new_images_2_19/results_gray/"
     for root, dirs, files in os.walk(test_path):
         item = gray_image
         if item.endswith(".jpg") or item.endswith(".JPG"):
             x = os.path.join(root, item)
             im = cv2.imread(x)
             imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-            print item
+            # print item
             ret, thresh = cv2.threshold(imgray, 25, 255, 0)
             # Find the contours
             contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -56,7 +58,7 @@ def contour_finding(gray_image):
 
             # Drawing the Largest contour on the image
             cv2.drawContours(im, cmt, -1, (0, 255, 0), 3)
-            cv2.imwrite('rice_test/new_images_2_17/results_contour/' + item, im)
+            cv2.imwrite('rice_test/new_images_2_19/results_contour/' + item, im)
 
             ####PROCESSING THE LARGEST CONTOUR:
             ##The col_width and the row_height will be used to determine the tracking rectangle.
@@ -77,11 +79,11 @@ def contour_finding(gray_image):
             row_cmt = list_sorted(row_cmt)
             row_height = row_cmt[len(row_cmt) - 1] - row_cmt[0]
 
-            return row_cmt[0], col_cmt[0]
+            return row_cmt[0], col_cmt[0], len(col_cmt), row_height, col_width
 
 
-test_path = "rice_test/new_images_2_17/images/"
-roi = cv2.imread('rice_test/new_images_2_17/sauce/sauce.png')
+test_path = "rice_test/new_images_2_19/images/"
+roi = cv2.imread('rice_test/new_images_2_19/sauce/sauce.png')
 hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
 for root, dirs, files in os.walk(test_path):
@@ -101,7 +103,7 @@ for root, dirs, files in os.walk(test_path):
             # print dst
 
             # match = cv2.compareHist(roihist,inputImage,method=0)
-            # my_file = open('rice_test/new_images_2_17/txt_results/match_3.txt','a')
+            # my_file = open('rice_test/new_images_2_19/txt_results/match_3.txt','a')
             # my_file.write(item+': '+str(match)+'\n')
             # my_file.close()
 
@@ -116,8 +118,8 @@ for root, dirs, files in os.walk(test_path):
 
             # res = np.vstack((target,thresh,res))
             # cv2.imwrite('test/back_project_test/test4.png',res)
-            cv2.imwrite("rice_test/new_images_2_17/results/" + item, res)
-            print 'item: ' + item + ' done'
+            cv2.imwrite("rice_test/new_images_2_19/results/" + item, res)
+            # print 'item: ' + item + ' done'
             # if match > 0.005:
             #   my_file = open('test/back_project_test/txt_results/high_match.txt','a')
             #  my_file.write(item+': '+str(match)+'\n')
@@ -125,19 +127,32 @@ for root, dirs, files in os.walk(test_path):
             # cv2.imwrite("test/back_project_test/txt_results/"+item, res)
             color_res = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
             gray_res = cv2.cvtColor(color_res, cv2.COLOR_BGR2GRAY)
-            cv2.imwrite("rice_test/new_images_2_17/results_gray/" + item, gray_res)
+            cv2.imwrite("rice_test/new_images_2_19/results_gray/" + item, gray_res)
 
             # Meanshift
 
-            r, c = contour_finding(item)
-            w, h = 780, 780
-            track_window = (c, r, w, h)
-            term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 10)
-            ret, track_window = cv2.meanShift(dst, track_window, term_crit)
+            r, c, length_contour, row_height, col_width = contour_finding(item)
+            if length_contour > 1350:
+                w, h = 780, 780
+                track_window = (c, r, w, h)
+                term_crit = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 10)
+                ret, track_window = cv2.meanShift(dst, track_window, term_crit)
 
-            # Draw it on image
-            x, y, w, h = track_window
-            cv2.rectangle(target, (x, y), (x + w, y + h), 255, 6)
-            cv2.imwrite("rice_test/new_images_2_17/results_mean_shift/" + item, target)
+                # Draw it on image
+                x, y, w, h = track_window
+                cv2.rectangle(target, (x, y), (x + w, y + h), 255, 6)
+                cv2.imwrite("rice_test/new_images_2_19/results_mean_shift/" + item, target)
+                my_file = open('rice_test/new_images_2_19/txt_results/length_contour.txt', 'a')
+                #my_file.write(item + ': ' + str(length_contour) + '\n')
+                my_file.write(item + ': ' + str(length_contour) + ', row_height: ' + str(row_height) +', col_width: ' + str(col_width) + '\n')
+                my_file.close()
+            elif length_contour <= 1200:
+                cv2.imwrite("rice_test/new_images_2_19/no_result_mean_shift/" + item, target)
+                print 'hello_not_printed'
+                my_file = open('rice_test/new_images_2_19/txt_results/length_contour_no_result.txt', 'a')
+                #my_file.write(item + ': ' + str(length_contour) + '\n')
+                my_file.write(item + ': ' + str(length_contour) + ', row_height: ' + str(row_height) +', col_width: ' + str(col_width) + '\n')
+                my_file.close()
 
 print("--- %s seconds ---" % (time.time() - start_time))
+
